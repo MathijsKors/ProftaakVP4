3aebdbe7207f2a73952ca6c32d4883fe86ede99c
Dennis van Hest <dhbhest@avans.nl>
2016-05-11 12:40:03 +0200

    GUI 0.2

--- Presentation/SysteemUI.java
+++ Presentation/SysteemUI.java
@@ -1,10 +1,14 @@
 package Presentation;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Frame;
+import java.awt.Insets;
+import java.util.ArrayList;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 /**
  *
@@ -17,28 +21,56 @@ public class SysteemUI extends JFrame
     public SysteemUI()
     {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setExtendedState(Frame.MAXIMIZED_BOTH);
         setVisible(true);
         
         setLayout(new BorderLayout());
+        setTitle("De Hartige Hap");
+        
         add(new navBarPanel(), BorderLayout.NORTH);
+        
+        pack();
+        setExtendedState(Frame.MAXIMIZED_BOTH);
     }
     
     class navBarPanel extends JPanel
-    {
-        private JButton menuButton, orderedItemButton, billButton, helpButton;
+    {   
+        private JButton helpButton;
         
         public navBarPanel()
         {
-            menuButton = new JButton("Menu");
-            orderedItemButton = new JButton("Bestelde Gerechten");
-            billButton = new JButton("Rekening");
-            helpButton = new JButton("?");
+            setLayout(new BorderLayout());
             
-            add(menuButton);
-            add(orderedItemButton);
-            add(billButton);
-            add(helpButton);
+            add(new navButtonPanel(), BorderLayout.WEST);
+            
+            add(helpButton, BorderLayout.EAST);
+        }
+        
+        class navButtonPanel extends JPanel
+        {
+            private JButton menuButton, orderedItemButton, billButton;
+            private ArrayList<JButton> navBarButtons;
+            
+            public navButtonPanel()
+            {
+                //Menu buttons
+                menuButton = new JButton("Menu");
+                orderedItemButton = new JButton("Bestelde Gerechten");
+                billButton = new JButton("Rekening");
+                helpButton = new JButton("?");
+                
+                //Array with all navbar buttons for styling and adding to the panel
+                navBarButtons = new ArrayList<>();
+                
+                navBarButtons.add(menuButton);
+                navBarButtons.add(orderedItemButton);
+                navBarButtons.add(billButton);
+                
+                for(JButton button : navBarButtons)
+                {
+                    button.setMargin(new Insets(10, 10, 10, 10));
+                    add(button);
+                }
+            }
         }
     }
 }
